640896bbecef28aab8a598acc58703cacd9d56b2
Mriganka <mrigankapauloracle@gmail.com>
2023-04-23 18:44:40 +0530

    Footer Hidden

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter} from 'react-router-dom';
 import { About, Contact, Experience, Feedbacks, 
   Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 import Certificates from './components/Certificates';
-import Footer from './components/footer';
+//import Footer from './components/footer';
 
 const App=() => {
   return (
